ab7e7b0bd9a50f67f105f791013dce22206b08f8
Dziugas <dziugas@graphity.org>
2017-03-30 20:09:42 +0300

    Join skiltyje updatinu Contacts

---
+++ js/myApp.js
@@ -0,0 +1,6 @@
+/**
+ * Created by User on 2017-03-30.
+ */
+var app = angular.module('myApp', []);
+
+app.controller('myController', myController);
---
+++ js/myController.js
@@ -0,0 +1,17 @@
+/**
+ * Created by User on 2017-03-30.
+ */
+function myController() {
+    var vm=this;
+    vm.vardas ='Džiugas';
+    vm.masyvas = [
+        {
+            "name" : "Džiugas Tornau",
+            "email" : "dziugas@lay-up.com"
+        },
+        {
+            "name" : "Ignas Jančiukas",
+            "email" : "ignas@lay-up.com"
+        }
+    ]
+}
